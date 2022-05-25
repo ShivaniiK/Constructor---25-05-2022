6328c8ad492b30f5ee3b3cf446bbc59bf4cbb38f
ShivaniiK <105714949+ShivaniiK@users.noreply.github.com>
2022-05-25 08:47:17 -0700

    Add files via upload

---
+++ ConstructorJava.java
@@ -0,0 +1,27 @@
+package com.constructor;
+
+public class ConstructorJava {
+
+	public ConstructorJava(String strUrl) {
+		System.out.println("Open Browser :"+strUrl);
+	}
+
+    public void firstName(String strfirstName) {
+    	System.out.println("First name :"+strfirstName);
+    }
+    
+    public void lastName(String strlastName) {
+    	System.out.println("First name :"+strlastName);
+    }
+    
+
+    public void mobileNumber(String strMobileNumber) {
+    	System.out.println("First name :"+strMobileNumber);
+    }
+   
+
+    public void closeBrowser() {
+    	System.out.println("Closing Browser");
+    }
+    }
+
---
+++ TestJavaConstructor.java
@@ -0,0 +1,15 @@
+package com.constructor;
+
+public class TestJavaConstructor {
+
+	public static void main(String[] args) {
+		
+		ConstructorJava objConstructorJava = new ConstructorJava("https://www.facebook.com/");
+		objConstructorJava.firstName("Shivani");
+		objConstructorJava.lastName("Kaingade");
+		objConstructorJava.mobileNumber("9835592629");
+		objConstructorJava.closeBrowser();
+
+	}
+
+}
